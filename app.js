8b783a151056c066611ed3bcf95b72b453e6a455
MUHUSIN-16 <muhusin161099@gmail.com>
2023-10-06 14:49:58 +0530

    changes has been done

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require('express');
 const connecDb = require("./config/dbConnection")
 const bodyParser = require('body-parser');
 const errorHandler = require('./middleware/errorHandler');
-const axios = require("axios")
+const cors = require("cors")
+
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 8080 ;
+const PORT = process.env.PORT || 8080;
+app.use(cors({
+  origin:true
+}));
 
 app.use(bodyParser.json());
 app.use(errorHandler)
 app.use(express.json())
 
 app.use("/api/user", require("./routes/userRoutes"));
-app.use("/api", require("./routes/taskRoutes"))
+app.use("/api", require("./routes/taskRoutes"));
   
 connecDb();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
