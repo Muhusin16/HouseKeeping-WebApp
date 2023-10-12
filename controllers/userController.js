7b0b27e55192eed3286e5dcc4bfbd04fa7fec3ea
Muhusin16 <muhusin@zool.in>
2023-10-12 10:33:50 +0530

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ const registerUser = async (req, res) => {
     return res.status(400).json({ message: 'User already exists' });
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10,);
 
   const newUser = await User.create({
     username,
@@ -20,12 +20,14 @@ const registerUser = async (req, res) => {
     password: hashedPassword,
     
   });
-
   console.log(`User created ${newUser}`);
-
-  await newUser.save();
-
-  res.status(201).json({ id: newUser.id, email: newUser.email });
+  if (newUser) {
+    res.status(201).json({id: newUser.id, email: newUser.email});
+}
+else {
+    res.status(400);
+    throw new Error("User data is not Valid");
+}
 };
 
 const loginUser = async (req, res) => {
