ddf4f56afd91d88d93a2d1bc344378150c683faa
Muhusin16 <muhusin@zool.in>
2023-10-16 14:42:36 +0530

    commit

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,21 +1,17 @@
 const express = require("express");
 
 const router = express.Router();
-const {createRoom, getallRoom, getoneRoom, updateRoom, deleteRoom   } = require("../controllers/taskControllers")
+const { createHall, createConference, createKitchen, createReception, createWashroom,
+getHall, getConference, getWashroom, getkitchen,getreception  } = require("../controllers/taskControllers")
 
-const isAdmin = require("../middleware/adminHandler")
 
-//router.post('/complete-task/:taskId', completeTask)
+router.post('/hall', createHall ).post('/conference', createConference).post('/kitchen', createKitchen).post('/reception', createReception).post('/washroom', createWashroom)
 
-router.post('/createroom', createRoom)
+router.get('/hall', getHall).get('/conference', getConference).get('/kitchen', getkitchen).get('/washroom', getWashroom).get('/reception', getreception)
 
-router.get('/getallroom',isAdmin, getallRoom)
-
-router.get('/getoneroom/:roomId', getoneRoom)
-
-router.put('/updateroom/:roomId', updateRoom)
-
-router.delete('/deleteroom/:roomId', deleteRoom)
+// router.get('/getoneroom/:roomId', getoneRoom)
+// router.put('/updateroom/:roomId', updateRoom)
+// router.delete('/deleteroom/:roomId', deleteRoom)
 
 module.exports = router
 
