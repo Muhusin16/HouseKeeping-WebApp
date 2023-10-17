96d3c095cdb1351593264c61bdc29058a3f9902f
Muhusin16 <muhusin@zool.in>
2023-10-17 10:21:55 +0530

    commit

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 const { createHall, createConference, createKitchen, createReception, createWashroom,
-getHall, getConference, getWashroom, getkitchen,getreception  } = require("../controllers/taskControllers")
+getHall, getConference, getWashroom, getKitchen,getReception  } = require("../controllers/taskControllers")
 
 router.post('/hall', createHall ).post('/conference', createConference).post('/kitchen', createKitchen).post('/reception', createReception).post('/washroom', createWashroom)
 
-router.get('/hall', getHall).get('/conference', getConference).get('/kitchen', getkitchen).get('/washroom', getWashroom).get('/reception', getreception)
+router.get('/hall', getHall).get('/conference', getConference).get('/kitchen', getKitchen).get('/washroom', getWashroom).get('/reception', getReception)
 
 // router.get('/getoneroom/:roomId', getoneRoom)
 // router.put('/updateroom/:roomId', updateRoom)
