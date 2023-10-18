cd82be1cdc95bd990c184d9e8ae58fd6d48541f7
Muhusin16 <muhusin@zool.in>
2023-10-18 16:59:48 +0530

    commit

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -4,14 +4,11 @@ const getRoomByUserId = async (req, res) => {
   const { user_id } = req.params;
   
   try {
-    // You can query each room type here
     const hall = await Hall.findOne({ user_id }).exec();
     const kitchen = await Kitchen.findOne({ user_id }).exec();
     const reception = await Reception.findOne({ user_id }).exec();
     const conference = await Conference.findOne({ user_id }).exec();
     const washroom = await Washroom.findOne({ user_id }).exec();
-
-    // You can customize the response structure as needed
     const rooms = {
       hall,
       kitchen,
