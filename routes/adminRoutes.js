5090a06a70bc941718c44c3a769ec05f211cccd3
Muhusin16 <muhusin@zool.in>
2023-10-18 17:14:24 +0530

    commit

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getAllUsers, adminUser, getUserById, updateUser, deleteUser} = require('../controllers/adminController');
 const isAdmin = require("../middleware/adminHandler")
 
-const { getRoomByUserId } = require('../controllers/roomController')
+//const { getRoomByUserId } = require('../controllers/roomController')
 
 router.post("/admin", adminUser);
 
