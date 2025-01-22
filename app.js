994ace1f3b49359660988be39b8f495ba955f985
Maruthigitzool <maruthi@zool.in>
2025-01-22 14:32:40 +0530

    app.js commented for testing

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const errorHandler = require('./middleware/errorHandler');
 const cors = require("cors");
 const morgan = require("morgan");
 const CooKieParser = require("cookie-parser");
-// const multer = require('multer');
-// const path = require("path")
-// const Category = require('./models/categoryModel');
 require("dotenv").config();
 
 const app = express();
@@ -20,25 +17,6 @@ app.use(errorHandler)
 app.use(express.json())
 app.use(CooKieParser())
 app.use(express.static("public"))
-
-// const storage = multer.diskStorage({
-//   destination:(req, file, cb) =>{
-//     cb(null, "public/Images")
-//   },
-//   filename:(req,file, cb) =>{
-//     cb(null, file.filename + "_" + Date.now() + path.extname(file.originalname))
-//   }
-// })
-// const upload = multer({
-//   storage: storage
-// })
-
-// app.post("/upload", upload.single('file'),(req, res) => {
-//   Category.create({image: req.file.filename})
-//   .then(result => res.json(result))
-//   .catch(err => console.log(err))
-// })
-
 
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api", require("./routes/taskRoutes"));
